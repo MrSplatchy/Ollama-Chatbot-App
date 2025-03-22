433abc158f1aad4a785ac27a1eacff2ff71181b1
Mr.Bradley <166937816+MrSplatchy@users.noreply.github.com>
2025-03-22 20:54:25 +0000

    AZ

--- fastapi/app.py
+++ fastapi/app.py
@@ -12,7 +12,7 @@ import asyncio
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Conversion de la fonction en asynchrone
-    await wait_for_ollama_tag(), health_check()
+    await wait_for_ollama_tag()
     yield
 
 app = FastAPI(lifespan=lifespan)
