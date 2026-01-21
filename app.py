8a1018eac17521a5648aed4b791f3253c967024e
MrSplatchy <mrbradley082011@gmail.com>
2026-01-21 19:47:26 +0100

    revert

--- app.py
+++ app.py
@@ -2,30 +2,21 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from contextlib import asynccontextmanager
+from model import generateResponse
 
-from model import generateResponse, model, url
-import requests
+app = FastAPI()
 
-@asynccontextmanager
-async def installModel(_:FastAPI):
-    requests.post(
-        f"{url}/api/pull",
-        json={"name": model},
-        timeout=300
-    )
-    yield
-    
-app = FastAPI(lifespan=installModel)
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-conversation_history= []
+conversation_history = []
 
 
 @app.get("/", response_class=HTMLResponse)
 def show_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/", response_class=HTMLResponse)
 async def ask(request: Request, prompt: str = Form(...)):
@@ -35,8 +26,10 @@ async def ask(request: Request, prompt: str = Form(...)):
     conversation_history.append({"role": "user", "content": prompt})
 
     try:
-        return StreamingResponse(generateResponse(prompt, conversation_history), media_type="text/plain")
-    
+        return StreamingResponse(
+            generateResponse(prompt, conversation_history), media_type="text/plain"
+        )
+
     except Exception as e:
         return StreamingResponse(iter([f"Error: {str(e)}"]), media_type="text/plain")
 
@@ -45,4 +38,3 @@ async def ask(request: Request, prompt: str = Form(...)):
 async def health_check():
     while True:
         return {"status": "ok"}
-
--- model.py
+++ model.py
@@ -1,31 +1,25 @@
-import ollama
+from ollama import chat
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
 model = getenv("MODEL")
-url = getenv('OLLAMA_HOST')
-async def generateResponse(content, chat_history: list): 
+
+
+async def generateResponse(content, chat_history: list):
     # LLM Preparations
-    client = ollama.Client(host=url)
-    response = client.chat(
-        model = model,
+    response = chat(
+        model=model,
         messages=chat_history,
         stream=True,
-
     )
-    llm_response = "" 
+    llm_response = ""
 
     # Stream the response and prepare it to be added to the chat history
     for chunk in response:
-        content = chunk['message']['content']
+        content = chunk["message"]["content"]
         if content:
             llm_response += content
             yield content
 
-
     chat_history.append({"role": "assistant", "content": llm_response})
-
-
-
-
